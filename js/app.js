05171a33cb2b5b3a3baa39537ea98a91ee2ad5e7
Anastasia Bondarenko <abondarenko@biarum.com>
2017-07-05 19:43:46 +0300

    Changed tabs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-function openTab(tabName) {
-    var i;
-    var x = document.getElementsByClassName("tabs-content__element");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-}
+$(document).ready(function(){
+
+    $('.tabs li button').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('.tabs li button').removeClass('active');
+        $('.tabs-content__element').removeClass('active');
+
+        $(this).addClass('active');
+        $("#"+tab_id).addClass('active');
+    })
+
+});
